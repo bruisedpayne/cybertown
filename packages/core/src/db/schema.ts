6116a05bf33b856e4323d6b9a6ffee45efe5de50
Bruised Payne <bruisedpayne@gmail.com>
2025-11-24 21:29:46 +0530

    added users table

--- packages/core/src/db/schema.ts
+++ packages/core/src/db/schema.ts
@@ -1,10 +1,18 @@
-import { pgTable, varchar, serial, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, varchar, serial, text, integer } from "drizzle-orm/pg-core";
+import { timestamps } from "./timestamps.js";
 
 export const usersTable = pgTable('users', {
 	id: serial("id").primaryKey(),
 	name: varchar("name", { length: 256 }).unique().notNull(),
 	displayName: varchar("display_name", { length: 256 }).notNull(),
 	avatar: varchar("avatar", { length: 256 }),
-	createdAt: timestamp("created_at").defaultNow(),
-	updatedAt: timestamp("updated_at").defaultNow(),
+	...timestamps,
+});
+
+export const roomsTable = pgTable('rooms', {
+	id: serial("id").primaryKey(),
+	topic: varchar("name", { length: 256 }),
+	languages: text("languages").array().notNull(),
+	size: integer("size").notNull(),
+	...timestamps,
 });
