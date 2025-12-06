b0d60a7d719f01f763c32dd83b77a7b56aa3318a
Bruised Payne <bruisedpayne@gmail.com>
2025-12-06 18:18:18 +0530

    Init db in fetch  handler

--- packages/app/src/server.ts
+++ packages/app/src/server.ts
@@ -1,8 +1,11 @@
 // server.ts
 import handler from '@tanstack/react-start/server-entry'
+import { initDB } from '@cybertown/core/db'
+import { env } from 'cloudflare:workers'
 
 export default {
   async fetch(request: Request) {
+    initDB(env.HYPERDRIVE)
     return handler.fetch(request)
   },
 }
