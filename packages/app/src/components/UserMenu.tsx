d0f938a5fab2459c0467d463e39715fadf469a96
Bruised Payne <bruisedpayne@gmail.com>
2025-12-13 12:26:41 +0530

    Show confirmation when logging out

--- packages/app/src/components/UserMenu.tsx
+++ packages/app/src/components/UserMenu.tsx
@@ -7,6 +7,17 @@ import {
 import { authClient } from '@/lib/utils'
 import { useQueryClient } from '@tanstack/react-query'
 import { useRouter } from '@tanstack/react-router'
+import * as React from 'react'
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from '@/components/ui/alert-dialog'
 
 type AuthUser = {
   id: string
@@ -19,35 +30,58 @@ type Props = {
 }
 
 export function UserMenu(props: Props) {
+  const [confirmOpen, setConfirmOpen] = React.useState(false)
   const queryClient = useQueryClient()
   const router = useRouter()
   const { user } = props
 
+  async function logOut() {
+    await authClient.signOut()
+    queryClient.removeQueries({ queryKey: ['user'] })
+    router.invalidate()
+    setConfirmOpen(false)
+  }
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="rounded-full border px-4 py-1.5 flex items-center gap-2.5">
-        {user.image && (
-          <img
-            src={user.image}
-            alt={user.name}
-            className="w-6 h-6 rounded-full"
-          />
-        )}
-        <p>{user.name}</p>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem className="py-1">Edit Profile</DropdownMenuItem>
-        <DropdownMenuItem
-          className="py-1"
-          onClick={async () => {
-            await authClient.signOut()
-            queryClient.removeQueries({ queryKey: ['user'] })
-            router.invalidate()
-          }}
-        >
-          Log out
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="rounded-full border px-4 py-1.5 flex items-center gap-2.5">
+          {user.image && (
+            <img
+              src={user.image}
+              alt={user.name}
+              className="w-6 h-6 rounded-full"
+            />
+          )}
+          <p>{user.name}</p>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem className="py-1">Edit Profile</DropdownMenuItem>
+          <DropdownMenuItem
+            className="py-1"
+            onClick={() => setConfirmOpen(true)}
+          >
+            Log out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <AlertDialog open={confirmOpen} onOpenChange={setConfirmOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Confirmation</AlertDialogTitle>
+            <AlertDialogDescription>
+              Are you sure you want to log out?
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={() => logOut()}>
+              Log out
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   )
 }
